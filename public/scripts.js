05b4a733459ed8e36c48ec5c130a40b01f2aa224
Harish Malakar <harishmalakar19@gmail.com>
2024-07-31 14:40:14 +0530

    server api added

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,5 @@
 //io() connects to the socket io server
-const socket=io('http://localhost:4000',{
+const socket=io('https://express-ws-server.vercel.app/',{
     auth: {
         token: "abcd"
       },
